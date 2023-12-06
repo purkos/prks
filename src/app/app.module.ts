943f0c2c29ffe7d838167a674069838c8acea2c0
Jakub Piórkowski <kuba.piorkos1@o2.pl>
2023-12-06 20:48:10 +0100

    Created project

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectItemComponent } from './projects/project-item/project-item.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ProjectItemComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}",],
+  theme: {
+    extend: {
+      colors:{
+        richBlack: "#0D0C1D",
+        oxfordBlue: "#12152B",
+        ultraFiolet: "#474973",
+        roseQuartz: "#A69CAC",
+        almond: "#F1DAC4"
+      }
+    },
+  },
+  plugins: [],
+}
+
